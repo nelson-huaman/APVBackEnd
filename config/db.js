4403dd89fa0fa2a429c402a99af8dcffa029d43e
Nelson Huamán H. <libra.nelsonhuaman@gmail.com>
2024-12-01 14:51:08 -0500

    -- fix 2 --

--- config/db.js
+++ config/db.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
    try {
-      const db = await mongoose.connect(process.env.MONGO_URI);
+      const db = await mongoose.connect(process.env.MONGO_URI, {
+         dbName: process.env.DB_NAME
+      });
       const url = `${db.connection.host}:${db.connection.port}`;
       console.log(`MongoDB conectado en: ${url}`);
    } catch (error) {
-      console.log(`Error: ${error.message}`);
+      console.error(`Error al conectar a MongoDB: ${error.message}`);
       process.exit(1);
    }
 }
